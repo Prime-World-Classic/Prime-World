d7e068ee1c7a7be12c388f634c2867e1deab1bf5
Rekongstor <rekongstor@mail.ru>
2025-04-22 02:08:02 +0300

    Update everything what remains

--- pw/branches/r1117/Data/GameLogic/Scripts/DPStest_ForDesigners.lua
+++ pw/branches/r1117/Data/GameLogic/Scripts/DPStest_ForDesigners.lua
@@ -30,15 +30,3 @@ function ShowHeroLevel (heroName)
 	LuaShowTutorialOvertipLevelHighlight ( heroName, true )
 	WaitForUnitsDead( heroName )
 end
-
-function Init( reconnecting )
-	if not reconnecting then	
-		for team = 0, 1 do
-			for hero = 0, 4 do
-				currentHero = tostring(team) .. tostring(hero)
-				LuaHeroTakeConsumable ( currentHero, "RemoveCooldown" ) -- прописан в dictionary 
-			end
-		end
-				
-	end
-end
