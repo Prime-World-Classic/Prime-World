f2e806eb79c51641dddda892f190a437d9ef4e60
Rekongstor <rekongstor@mail.ru>
2025-05-05 22:29:40 +0300

    Updated make_links and moved to the root folder

---
+++ make-links.bat
@@ -0,0 +1,4 @@
+mklink /j /D pw_publish\branch\Client\PvP\Data .\pw\branches\r1117\Data\
+mklink /j /D pw_publish\branch\Client\PvP\Localization .\pw\branches\r1117\Localization\
+mklink /j /D pw_publish\branch\Client\PvP\Profiles  .\pw\branches\r1117\Profiles\
+mklink /j /D pw_publish\branch\Client\PvP\Tools  .\pw\branches\r1117\Tools\
--- pw_publish/branch/Client/PvP/make-links.bat
+++
@@ -1,4 +0,0 @@
-mklink /j /D Data C:\Users\rekon\source\repos\Prime-World-holydel\pw\branches\r1117\Data\
-mklink /j /D Localization C:\Users\rekon\source\repos\Prime-World-holydel\pw\branches\r1117\Localization\
-mklink /j /D Profiles  C:\Users\rekon\source\repos\Prime-World-holydel\pw\branches\r1117\Profiles\
-mklink /j /D Tools  C:\Users\rekon\source\repos\Prime-World-holydel\pw\branches\r1117\Tools\
