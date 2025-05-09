37d89a690d2159e289821c61f09ba15d8054e263
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2025-05-09 19:50:04 +0400

    update.sh use portable git

--- update.sh
+++ update.sh
@@ -6,32 +6,36 @@ TEST_DIR="${WD}/Game"
 cd "$TEST_DIR" 2>/dev/null || WD="$WD/.."
 
 GAME_DIR="${WD}/Game"
-GAME_REMOTE="https://github.com/Prime-World-Classic/PWCGitUpdates-test.git"
+GAME_REMOTE="https://github.com/Prime-World-Classic/PWCGitUpdates.git"
 LAUNCHER_DIR="${WD}/Launcher/content"
-LAUNCHER_REMOTE="https://github.com/Prime-World-Classic/content-test.git"
+LAUNCHER_REMOTE="https://github.com/Prime-World-Classic/content.git"
+
+# We use our portable git (appImage) because Lutris in Flatpak cannot use system git
+# and Proton-GE also, in Conty
+GIT="${WD}/git"
 
 echo "Updating Game..."
 
 cd "$GAME_DIR" || exit 12
 
 if [ ! -d .git ]; then
-    git init
+    $GIT init
     echo "Game repo has been initialized"
 else
     echo "Game repo is already exists, no need to init"
 fi
 
-git remote add origin "$GAME_REMOTE" 2>/dev/null
+$GIT remote add origin "$GAME_REMOTE" 2>/dev/null
 if [ $? -eq 0 ]; then
     echo "Game repo remote has been added"
 else
-    git remote set-url origin "$GAME_REMOTE"
+    $GIT remote set-url origin "$GAME_REMOTE"
     echo "Game repo remote has been updated to $GAME_REMOTE"
 fi
 
 echo "Updating game files"
-git fetch --all --progress
-git reset --hard origin/main
+$GIT fetch --all --progress
+$GIT reset --hard origin/main
 
 if [ $? -eq 0 ]; then
     echo "Successfully updated game files"
@@ -48,23 +52,26 @@ echo "Updating launcher"
 cd "$LAUNCHER_DIR" || exit 13
 
 if [ ! -d .git ]; then
-    git init
+    $GIT init
     echo "Launcher repo has been initialized"
 else
     echo "Launcher repo is already exists, no need to init"
 fi
 
-git remote add origin "$LAUNCHER_REMOTE" 2>/dev/null
+$GIT remote add origin "$LAUNCHER_REMOTE" 2>/dev/null
 if [ $? -eq 0 ]; then
     echo "Launcher repo remote has been added"
 else
-    git remote set-url origin "$LAUNCHER_REMOTE"
+    $GIT remote set-url origin "$LAUNCHER_REMOTE"
     echo "LAUNCHER repo remote has been updated to $LAUNCHER_REMOTE"
 fi
 
 echo "Updating launcher files"
-git fetch --all --progress
-git reset --hard origin/main
+$GIT branch -m main
+$GIT branch -v
+$GIT fetch --all --progress
+$GIT reset --hard origin/main
+$GIT branch -v
 
 if [ $? -eq 0 ]; then
     echo "Successfully updated game files"
