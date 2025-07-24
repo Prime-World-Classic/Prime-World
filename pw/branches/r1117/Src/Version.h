0a91cd67b2380b304cf4765790bb31e011b76cc3
aigiz010 <aigiz074@gmail.com>
2025-07-24 12:02:58 +0300

    фикс Звезд, Время респавна на тренировке - 5 сек

--- pw/branches/r1117/Src/Version.h
+++ pw/branches/r1117/Src/Version.h
@@ -11,7 +11,7 @@
 
 #define VERSION_MAJOR    2
 #define VERSION_MINOR    6
-#define VERSION_PATCH    4
+#define VERSION_PATCH    5
 #define VERSION_BUILD    0
 #define VERSION_REVISION 0
 #define VERSION_REVISION_PATCHED 0
