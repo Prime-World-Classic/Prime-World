6f79d54ff09df227400817d9b95ba635e02b0e3d
Bogdan <81803584+ifstz@users.noreply.github.com>
2025-10-02 10:17:17 +0300

    Переписал механизм условий завершения турнира

--- pw/branches/r1117/Data/GameLogic/Scripts/Tournament.lua
+++ pw/branches/r1117/Data/GameLogic/Scripts/Tournament.lua
@@ -3,52 +3,114 @@ include ("GameLogic/Scripts/StatesManager.lua")
 include ("GameLogic/Scripts/Common.lua")
 include ("GameLogic/Scripts/Consts.lua")
 
-FINISH = false
+IS_FINAL = false
+LIMIT_SCORE = 150
+PRICE_HERO = 50
 
-function Init( reconnecting )
-
+function Init()
 	
+	LuaMessageToChat("Tournament for Prime World Classic by ifst 1.0.0")
+	
+	InitQuest()
 	
 end
 
-function OnUnitDie( victimId, killerId, lastHitterId, deathParamsInfo )
+function OnUnitDie()
 
-	if FINISH then 
+	if IS_FINAL then 
 		
 		return
 	
 	end
 	
-	if killerId == -1 then
+	local A, B = GetScore()
 	
-		return
+	UpdateQuest( A, B )
+	
+	if A > B then 
+		
+		CheckFinalA( A )
+		
+	else
+		
+		CheckFinalB( B )
 		
 	end
 	
-	if( LuaGetUnitTypeById( killerId ) == UnitTypeHeroMale and not LuaHeroIsCloneById( victimId ) ) then
-		
-		local heroNameId = LuaGetUnitObjectNameById(killerId)
-		
-		local faction = LuaGetUnitFaction( heroNameId )
-		
-		local kills = LuaStatisticsGetTotalNumHeroKills(heroNameId)
-		
-		if kills > 1 then 
-			
-			FINISH = true
-			
-			if faction == 1 then 
+end
+
+function GetScore()
+
+	local A = 0
 	
-				LuaKillUnit("MainB")
+	local B = 0
+
+	for team = 0, 1 do
+	
+		for hero = 0, 1 do
 		
-			else 
+			local heroNameId = tostring( team ) .. tostring( hero )
 		
-				LuaKillUnit("MainA")
-		
+			local TotalNumHeroKills = LuaStatisticsGetTotalNumHeroKills( heroNameId )
+			
+			local KillsTotal = LuaHeroGetKillsTotal( heroNameId )
+			
+			local UnitFaction = LuaGetUnitFaction( heroNameId )
+			
+			if UnitFaction == 1 then 
+				
+				A = ( A + ( TotalNumHeroKills * PRICE_HERO ) + KillsTotal )
+				
+			else
+				
+				B = ( B + ( TotalNumHeroKills * PRICE_HERO ) + KillsTotal )
+				
 			end
 		
 		end
+	
+	end
+	
+	return A, B
+	
+end
+
+function CheckFinalA( score )
+
+	if score >= LIMIT_SCORE then 
+	
+		IS_FINAL = true
+		
+		LuaKillUnit("MainB")
 		
 	end
+	
+end
+
+function CheckFinalB( score )
+
+	if score >= LIMIT_SCORE then 
+		
+		IS_FINAL = true
+		
+		LuaKillUnit("MainA")
+		
+	end
+	
+end
+
+function InitQuest()
+
+	LuaAddSessionQuest( "Q_A" )
+	
+	LuaAddSessionQuest( "Q_B" )
+
+end
+
+function UpdateQuest( A, B )
+	
+	LuaUpdateSessionQuest( "Q_A", A )
+	
+	LuaUpdateSessionQuest( "Q_B", B )
 	
 end
