71883945e576d06515d35355e857680a8ead8f2f
aigiz010 <aigiz074@gmail.com>
2025-11-07 16:39:43 +0300

    Revert "Визуальный ластхит для пограничья"
    This reverts commit bcef559da4a54040c7be74ebd0aac116e3ca19c6.
    
    # Conflicts:
    #	pw/branches/r1117/Data/GameLogic/Scripts/Moba.lua

--- pw/branches/r1117/Data/GameLogic/Scripts/Moba.lua
+++ pw/branches/r1117/Data/GameLogic/Scripts/Moba.lua
@@ -3,15 +3,7 @@ include ("GameLogic/Scripts/StatesManager.lua")
 include ("GameLogic/Scripts/Common.lua")
 include ("GameLogic/Scripts/Consts.lua")
 
-VISUAL_LAST_HIT = true
-
 function OnUnitDie( victimId, killerId, lastHitterId, deathParamsInfo )
-	
-	if VISUAL_LAST_HIT then 
-		
-		AddTriggerTop( PointReceived, victimId, killerId )
-	
-	end
 
 	if LuaGetUnitVariableById( victimId, "Boss_Dragon" ) ~= 0 then
 		local dbid = "Dragon"
@@ -33,26 +25,4 @@ function OnUnitDie( victimId, killerId, lastHitterId, deathParamsInfo )
 	else
 		return
 	end
-end
-
-function PointReceived( victimId, killerId )
-
-	if LuaGetLocalHero() == LuaGetUnitObjectNameById( killerId ) then 
-	
-		local victim = LuaGetUnitObjectNameById( victimId )
-		
-		if victim ~= "" then 
-		
-			local effectId = "PointReceivedEffectId_" .. victim
-			
-			LuaPlaceAttachedEffect( effectId, "PointReceived", victim )
-			
-			WaitState( 3 )
-			
-			LuaRemoveStandaloneEffect( effectId )
-			
-		end
-		
-	end
-
 end
