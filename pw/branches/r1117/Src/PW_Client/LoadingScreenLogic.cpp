c17f43ea454a0001bb8103f76bea74dd7ffd5f84
aigiz010 <aigiz074@gmail.com>
2026-02-17 21:26:52 +0300

    Назаание Рейтинга

--- pw/branches/r1117/Src/PW_Client/LoadingScreenLogic.cpp
+++ pw/branches/r1117/Src/PW_Client/LoadingScreenLogic.cpp
@@ -516,7 +516,7 @@ void LoadingScreenLogic::SetHeroInfo( int userId, const HeroInfo & heroInfo, con
       //heroInfo.team == lobby::ETeam::Team1? NDb::FACTION_FREEZE: NDb::FACTION_BURN;
 
     flashInterface->SetHeroRaiting(userId, heroInfo.raiting, heroInfo.winDeltaRaiting, heroInfo.loseDeltaRaiting, heroInfo.isNovice,  rankCalculator->GetRankIcon(faction, rank), rankCalculator->GetRankName(faction, rank));
-    flashInterface->SetHeroRaitingAcc(userId, heroInfo.raitingAcc, heroInfo.winDeltaRaitingAcc, heroInfo.loseDeltaRaitingAcc, heroInfo.isNovice,  rankCalculator->GetRankIcon(faction, rankAcc), rankCalculator->GetRankName(faction, rank));
+    flashInterface->SetHeroRaitingAcc(userId, heroInfo.raitingAcc, heroInfo.winDeltaRaitingAcc, heroInfo.loseDeltaRaitingAcc, heroInfo.isNovice,  rankCalculator->GetRankIcon(faction, rankAcc), rankCalculator->GetRankName(faction, rankAcc));
   }
 }
 
